55e92a30a3984bd2e5b2bbdd03a5c5f4c728e8a8
mpratama17 <yogmhmmd17@gmail.com>
2020-12-22 18:55:22 +0700

    membuat file .java + source code

---
+++ src/test/java/com/mpratama17/mybot/MybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpratama17.mybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
